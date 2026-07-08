8482346e14d572b15e197b88e2ae4ed08e7ce3b6
Jeff Ching <chingor@google.com>
2026-07-08 11:19:50 -0700

    feat: bootstrap storage client (#286)

---
+++ Sources/GoogleCloudStorage/StorageClient.swift
@@ -0,0 +1,37 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
+import GoogleCloudAuth
+import GoogleCloudGax
+
+/// A client for the [Cloud Storage] data-plane operations.
+///
+/// Use this client to write (upload) and read (download) objects in the Cloud Storage service.
+///
+/// [Cloud Storage]: https://docs.cloud.google.com/storage
+public final class StorageClient: StorageClientProtocol {
+  public let inner: GoogleCloudGax.HTTPClient
+  public let options: StorageClientOptions
+
+  public init(_ options: StorageClientOptions = .init()) throws {
+    self.options = options
+    self.inner = try GoogleCloudGax.HTTPClient(
+      from: options.client, withDefaultEndpoint: "https://storage.googleapis.com")
+  }
+}
---
+++ Sources/GoogleCloudStorage/StorageClientOptions.swift
@@ -0,0 +1,34 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import GoogleCloudGax
+
+/// Configuration options for ``StorageClient``.
+public struct StorageClientOptions: Sendable {
+  /// Common options used by all Google Swift SDK clients.
+  public var client: GoogleCloudGax.ClientOptions
+
+  public init(
+    client: GoogleCloudGax.ClientOptions = .init(),
+  ) {
+    self.client = client
+  }
+
+  /// Override specific values using closure modification.
+  public func with(_ config: (inout Self) -> Void) -> Self {
+    var copy = self
+    config(&copy)
+    return copy
+  }
+}
